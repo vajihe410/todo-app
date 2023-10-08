8e629aeab43b5635111ec671b547c50062c50f7f
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2023-10-08 14:28:06 +0330

    get task and date and show alert

---
+++ script.js
@@ -0,0 +1,37 @@
+const taskInput = document.getElementById("add-task")
+const dateInput = document.getElementById("add-date")
+const addButtion = document.getElementById("add-button")
+const showStatus = document.getElementById("show-status")
+
+const showAlert = (text,type) =>{
+    showStatus.innerHTML=""
+    const message = document.createElement("p");
+    message.innerText = text;
+    message.classList.add("alert");
+    message.classList.add(`${type}`);
+    showStatus.appendChild(message)
+    setInterval(()=>{showStatus.style.display="none"},2000)
+};
+
+const todos = []
+
+const addTask = () => {
+    const task = taskInput.value
+    const date = dateInput.value
+    const todo = {
+        task : task,
+        date : date,
+        completed : false,
+    }
+    if(!task){
+        showAlert("please enter todo","error")
+    }
+    else{
+        todos.push(todo)
+        taskInput.value = "";
+        dateInput.value = "";
+        showAlert("todo added successfullu","success")
+    }
+}
+
+addButtion.addEventListener("click",addTask)
